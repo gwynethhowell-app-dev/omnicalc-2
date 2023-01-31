f94a5b84083abc1c13962b406e86d1543f3d0140
gwynethhowell <ghowell@uchicago.edu>
2023-01-31 05:36:36 +0000

    working omnicalc

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,6 +1,4 @@
 class MathController < ApplicationController
-  #def minus_results 
-    #render({ :template => })
 
   def add
     render({ :template => "math_templates/add.html.erb"})
@@ -16,5 +14,30 @@ class MathController < ApplicationController
 
   def subtract
     render({ :template => "math_templates/subtract.html.erb"})
+  end
+  def minus_results
+  @subtract_this = params.fetch("subtract_this").to_f
+  @from_this = params.fetch("from_this").to_f
+  @result = @from_this - @subtract_this
+    render({ :template => "math_templates/sub_response.html.erb"}) 
+  end
+
+  def multiply
+    render({ :template => "math_templates/multiply.html.erb"})
+  end
+  def mult_results
+    @mult_this = params.fetch("mult_this").to_f
+    @by_this = params.fetch("by_this").to_f
+    @result = @mult_this * @by_this
+    render({ :template => "math_templates/mult_results.html.erb"})
+  end
+  def divide
+    render({ :template => "math_templates/divide_new.html.erb"})
+  end
+  def div_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
+    render({ :template => "math_templates/div_results.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get("/add", { :controller => "math", :action => "add"})
   get("/wizard_add", { :controller => "math", :action => "add_response"})
   get("/subtract", { :controller => "math", :action => "subtract"})
-
-  #get("/wizard_subtract", { :controller => "math", :action => ""})
+  get("/wizard_subtract", { :controller => "math", :action => "minus_results"})
+  get("/multiply", { :controller => "math", :action => "multiply"})
+  get("/wizard_multiply", { :controller => "math", :action => "mult_results"})
+  get("/divide", { :controller => "math", :action => "divide"})
+  get("/wizard_divide", { :controller => "math", :action => "div_results"})
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
